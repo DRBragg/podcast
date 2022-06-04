e79479e9ad0911a2610ee7529459a698f88d5a0d
Drew Bragg <dbragg@within3.com>
2022-06-03 23:32:53 -0400

    initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,6 @@
+import "index.css"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
